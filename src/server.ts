6aaf9c37aba556478c45decf402c1b4a9ce6a1ef
Vikash Kumar <vikash.kumar3@whitehatjr.com>
2023-06-27 16:54:33 +0530

    server set-up done

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import router from './interfaces/router';
+import 'dotenv/config'
+import sequelize from './config/database';
+import logger from './interfaces/middlewares/logger';
+
+const PORT = process.env.PORT
+const app = express();
+
+app.use(router);
+
+function handleShutdown(): void {
+    logger.info('shutting down node');
+    process.exit(0);
+}
+process.on('SIGINT', handleShutdown);
+process.on('SIGTERM', handleShutdown);
+
+sequelize.sync()
+    .then(() => logger.info('Connected successfully'))
+    .catch((error) => logger.error(`Unable to connect due to ${error}`))
+
+app.listen(PORT, () => {
+    logger.info(`Server started on ${PORT}`)
+})
+
+export default app;
